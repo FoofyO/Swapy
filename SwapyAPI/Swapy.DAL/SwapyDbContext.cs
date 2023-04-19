2fe8d5e38a280b0302ac5f4b2e23ab58f6734e8e
FoofyO <wan345sww344@gmail.com>
2023-04-19 23:35:53 +0400

    Setup Project
    Installing libraries, configure CQRS and Data Access Layer architecture
    
    Co-Authored-By: AxmedStark <43389495+AxmedStark@users.noreply.github.com>
    Co-Authored-By: OrxanRzayev <126977528+OrxanRzayev@users.noreply.github.com>

---
+++ SwapyAPI/Swapy.DAL/SwapyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Swapy.DAL
+{
+    public class SwapyDbContext : IdentityDbContext<IdentityUser>
+    {
+        public SwapyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //modelBuilder.ApplyConfiguration<Task>(new TaskConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
