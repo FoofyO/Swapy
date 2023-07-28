4b21e08b992dc4d4fd2545e5cb6fccc179682297
FoofyO <wan345sww344@gmail.com>
2023-07-29 01:07:12 +0400

    Bug fix

--- SwapyWeb/src/app/shared/shared.module.ts
+++ SwapyWeb/src/app/shared/shared.module.ts
@@ -1,48 +1,3 @@
-<<<<<<< Updated upstream
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AuthComponent } from './auth/auth.component';
-import { ErrorComponent } from './error/error.component';
-import { CategoryTreeComponent } from './category-tree/category-tree.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { ProductCardComponent } from './product-card/product-card.component';
-import { CustomDatePipeEn } from '../core/pipes/custom-date-en.pipe';
-import { FormsModule } from '@angular/forms';
-import { ShopCardComponent } from './shop-card/shop-card.component';
-import { CategoryCardComponent } from './category-card/category-card.component';
-import { ShortNumberPipe } from '../core/pipes/short-number.pipe';
-import { DateOnlyPipe } from '../core/pipes/date-only.pipe';
-import { PhoneNumberPipe } from '../core/pipes/phone-number.pipe';
-
-@NgModule({
-  declarations: [
-    AuthComponent,
-    FooterComponent,
-    HeaderComponent,
-    ErrorComponent,
-    CategoryTreeComponent,
-    PaginationComponent,
-    ProductCardComponent,
-    CustomDatePipeEn,
-    ShortNumberPipe,
-    ShopCardComponent,
-    CategoryCardComponent,
-    DateOnlyPipe,
-    PhoneNumberPipe
-  ],
-  imports: [
-    FormsModule,
-    CommonModule,
-    RouterModule
-  ],
-  exports: [HeaderComponent, FooterComponent, ErrorComponent, PaginationComponent, ProductCardComponent, ShopCardComponent, CategoryCardComponent, ShortNumberPipe, DateOnlyPipe, PhoneNumberPipe],
-  providers: [PaginationComponent]
-})
-=======
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -87,5 +42,4 @@ import { SpinnerComponent } from './spinner/spinner.component';
   exports: [HeaderComponent, FooterComponent, ErrorComponent, PaginationComponent, ProductCardComponent, ShopCardComponent, CategoryCardComponent, ShortNumberPipe, DateOnlyPipe, PhoneNumberPipe, SpinnerComponent],
   providers: [PaginationComponent]
 })
->>>>>>> Stashed changes
 export class SharedModule { }
