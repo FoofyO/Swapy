ac6cb52fe73acffe6b5babb3c7a24ef2e6a547f8
Samed <77391241+FoofyO@users.noreply.github.com>
2023-10-14 15:07:04 +0400

    Angular version bug fix

--- SwapyWeb/src/environments/environment.prod.ts
+++ SwapyWeb/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://swapyapi.azurewebsites.net",
+  animalsApiUrl: "api/v1/Products/Animals",
+  authApiUrl: "api/v1/Auth",
+  autosApiUrl: "api/v1/Products/Autos",
+  categoriesApiUrl: "api/v1/Categories",
+  chatsApiUrl: "api/v1/Chats",
+  clothesApiUrl: "api/v1/Products/Clothes",
+  electronicsApiUrl: "api/v1/Products/Electronics",
+  itemsApiUrl: "api/v1/Products/Items",
+  productsApiUrl: "api/v1/Products",
+  realEstatesApiUrl: "api/v1/Products/RealEstates",
+  shopsApiUrl: "api/v1/Shops",
+  tvsApiUrl: "api/v1/Products/TVs",
+  usersApiUrl: "api/v1/Users",
+  blobUrl: "https://swapyblob.blob.core.windows.net"
 };
